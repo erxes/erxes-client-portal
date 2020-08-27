4108c6db64e1757999640587b80d25cfe81e4fb0
Enkhtuvshin0513 <enhtuwshinn@gmail.com>
2020-08-27 12:27:12 +0800

    add clear button

--- src/modules/common/components/Search.js
+++ src/modules/common/components/Search.js
@@ -2,32 +2,79 @@ import React from 'react';
 import router from '../utils/router';
 
 export default class Search extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.search = this.search.bind(this);
+    this.state = {
+      searchValue: props.searchValue || '',
+    };
   }
 
-  search(e) {
-    const { history } = this.props;
+  onChange = (e) => {
     const value = e.target.value;
 
-    router.setParams(history, { searchValue: value });
-  }
+    this.setState({
+      searchValue: value,
+    });
+  };
+
+  onSearch = () => {
+    const { history } = this.props;
+    const { searchValue } = this.state;
+
+    router.setParams(history, { searchValue });
+  };
+
+  onKeyDown = (e) => {
+    const { history } = this.props;
+    const { searchValue } = this.state;
+
+    if (e.key === 'Enter') {
+      router.setParams(history, { searchValue });
+    }
+  };
+
+  clearSearch = () => {
+    const { history } = this.props;
+
+    this.setState({
+      searchValue: '',
+    });
+
+    router.setParams(history, { searchValue: '' });
+  };
+
+  renderClearButton = () => {
+    const { searchValue } = this.state;
+
+    if (searchValue) {
+      return (
+        <div className="clear-icon">
+          <i onClick={this.clearSearch}>x</i>
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   render() {
-    const { searchValue } = this.props;
+    const { searchValue } = this.state;
 
     return (
       <div className="search-container">
         <div className={`search`}>
+          <i className="icon-search" onClick={this.onSearch}></i>
+          <i className="icon-search" onClick={this.onSearch}></i>
+
           <input
             autoFocus={true}
-            onChange={this.search}
+            onChange={this.onChange}
             placeholder="Search for articles"
             value={searchValue}
+            onKeyDown={this.onKeyDown}
           />
-          <i className="icon-search"></i>
+          {this.renderClearButton()}
         </div>
       </div>
     );
