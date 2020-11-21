3fbd27a2243b1abd228ada932d151e0bec9867e7
anu-ujin0404 <anu.b@nmma.co>
2020-11-21 23:19:01 +0800

    fix header

--- src/modules/layouts/components/Layout.js
+++ src/modules/layouts/components/Layout.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import Header from './Header';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import Header from "./Header";
+import { Container } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
+import queryString from "query-string";
 
 function Layout(props) {
+  const { location } = props;
+
+  const queryParams = queryString.parse(location.search);
+
   return (
     <div className="layout">
-      <Header />
+      <Header history={props.history} searchValue={queryParams.searchValue} />
 
       <Container className="main-body">{props.children}</Container>
     </div>
   );
 }
 
-export default Layout;
+export default withRouter(Layout);
