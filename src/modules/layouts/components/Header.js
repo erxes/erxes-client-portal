7d46b9059d0e3ca6fe0d9c65a70fafc50becf267
Enkhee0817 <enkhamgalan.b@nmma.co>
2021-06-03 18:21:53 +0800

    Header background url update

--- src/modules/layouts/components/Header.js
+++ src/modules/layouts/components/Header.js
@@ -3,10 +3,23 @@ import { Row, Col, Container } from 'react-bootstrap';
 import Search from '../../common/components/Search';
 import Logo from '../../../assets/images/logos/apu-logo.png';
 import { Link } from 'react-router-dom';
+export const getEnv = () => {
+  const envs = {};
 
+  for (const envMap of window.envMaps) {
+    envs[envMap.name] = localStorage.getItem(
+      `erxes_client_portal_env_${envMap.name}`
+    );
+  }
+
+  return envs;
+};
+
+const { REACT_APP_API_URL } = getEnv();
 function Header(props) {
+
   const { backgroundImage, color } = props.kbTopic || {};
-  const backImg = backgroundImage && `url(${backgroundImage})`;
+  const backImg = backgroundImage && `url(${REACT_APP_API_URL}/read-file?key=${backgroundImage})`;
   const backColor = color && color;
 
   return (
