0287ff31516cf04710c827c62af2be34e49c81d3
nandirb <nandir.be@gmail.com>
2021-07-22 18:41:31 +0800

    some improvements on rendertag

--- src/modules/knowledgeBase/components/ArticleDetail.js
+++ src/modules/knowledgeBase/components/ArticleDetail.js
@@ -202,35 +202,35 @@ class Detail extends React.Component {
     if (!articleDetail) {
       return null;
     }
-    const content  = articleDetail.content;
+    const content= articleDetail.content;
+    const dom = new DOMParser().parseFromString(content, 'text/html');
+    const nodes = dom.getElementsByTagName("h2");
+
     const tagged = [];
-    const regex =  /<h[1-6]>(.*?)<\/h[1-6]>/g;
-    if (
-      !content.length ||
-      !content.match(regex)
-    ) {
-      return null;
-    }
 
-    content
-      .match(regex)
-      .map((obj) => tagged.push(
-        obj.replace(/<[^>]*>?/gm, '')
-        ));
+    const addId = (array, isTag) => {
+      return array.forEach( el => {
+       let taggedItem;
+       if(!el.lastChild.innerText === String.fromCharCode(160)) {
+         el.children.length > 0 ? taggedItem = el.lastChild.innerText
+         : taggedItem = el.innerText;
+ 
+          el.setAttribute("id", taggedItem)
+          isTag && tagged.push(taggedItem);
+       } 
+     })
+   }
 
-    if(tagged.length === 0){
-        return null;
-    }
-    const  h2Array = [...document.querySelectorAll("h1, h2, h3, h4, h5, h6")];
-    h2Array.map( (el)=>
-      el.setAttribute("id",el.innerText))
+    const  h2Array = document.getElementsByTagName("h2");
+    addId([...nodes], true)
+    addId([...h2Array], false)
+
     return (
       <>
         <div className="page-anchor" id="anchorTag">
-          <h6>Холбоос</h6>
+          <h6>ХОЛБООС </h6>
           <Scrollspy items={tagged} currentClassName="active">
             {tagged.map((val, index) => (
-
               <li key={index} > 
                  <a href={`#${val}`} >
                  {val}
@@ -242,7 +242,6 @@ class Detail extends React.Component {
       </>
     );
   };
-
   
   render() {
     const { articleDetail, category, kbTopic } = this.props;
