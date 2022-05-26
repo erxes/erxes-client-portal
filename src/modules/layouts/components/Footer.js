bf3381f6987232cf4ef6936faa010dcd9c9543bb
Ariunzaya <ariunzaya@erxes.io>
2022-05-26 19:15:49 +0800

    change footer links

--- src/modules/layouts/components/Footer.js
+++ src/modules/layouts/components/Footer.js
@@ -15,18 +15,6 @@ function Footer(props) {
           <ul className="socials">
             <li>
               <a
-                href="https://fb.erxes.io/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="https://erxes.io/static/images/logo/fb-icon.png"
-                  alt="facebook"
-                />
-              </a>
-            </li>
-            <li>
-              <a
                 href="https://github.com/erxes"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -39,6 +27,90 @@ function Footer(props) {
             </li>
             <li>
               <a
+                href="https://discord.com/invite/aaGzy3gQK5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/discord-logo-white.png"
+                  alt="discord"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.crunchbase.com/organization/erxes-inc#/entity"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/cb-new.png"
+                  alt="logo cb"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://angel.co/erxes"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/angelList.png"
+                  alt="angel"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.f6s.com/erxes"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/f6s-new.png"
+                  alt="f6s"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.techinasia.com/companies/erxes-inc"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/techinasia-white.png"
+                  alt="techinasia white"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:info@erxes.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/mail.png"
+                  alt="mail"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://fb.erxes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/fb-icon.png"
+                  alt="facebook"
+                />
+              </a>
+            </li>
+            <li>
+              <a
                 href="https://twitter.com/erxeshq"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -46,6 +118,42 @@ function Footer(props) {
                 <img
                   src="https://erxes.io/static/images/logo/tw-icon.svg"
                   alt="twitter"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/company/erxes"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/linkedin-icon.svg"
+                  alt="linkedin"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCunYU3kJiiDsXGfB068BbDA"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/yt-icon.svg"
+                  alt="youtube"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://medium.com/erxes"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://erxes.io/static/images/logo/medium.png"
+                  alt="medium"
                 />
               </a>
             </li>
