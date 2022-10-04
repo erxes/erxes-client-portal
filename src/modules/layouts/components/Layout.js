83c6827c0d6c4ce692911e179a1b364df54a3dda
enkhzul.e <enkhzul.e@nmtec.co>
2022-10-04 19:23:32 +0800

    add form

--- src/modules/layouts/components/Layout.js
+++ src/modules/layouts/components/Layout.js
@@ -12,52 +12,80 @@ import { getEnv } from "../../../apolloClient";
 
 const { REACT_APP_TOPIC_ID } = getEnv();
 
-class Layout extends React.Component {
-  componentDidMount() {
-    window.erxesSettings = {
-      messenger: {
-        brand_id: "5fkS4v",
-      },
-    };
+const Layout = (props) => {
+  const { location, getKbTopicQuery, history, children, forms = [], headingSpacing } = props;
 
+  const settings = {
+    messenger: { brand_id: "5fkS4v" },
+    forms: [],
+  };
+
+  const formWidgetSource =
+    "https://w.office.erxes.io/build/formWidget.bundle.js";
+  //"http://localhost:3200/build/formWidget.bundle.js";
+
+  for (const form of forms) {
+    settings.forms.push({ brand_id: form.brand_id, form_id: form.form_id });
+  }
+
+  const erxesSettings = `window.erxesSettings = ${JSON.stringify(settings)}`;
+
+  useEffect(() => {
     (() => {
       const script = document.createElement("script");
       script.src = "https://w.office.erxes.io/build/messengerWidget.bundle.js";
       script.async = true;
-
+      script.key = Math.random().toString();
       const entry = document.getElementsByTagName("script")[0];
       entry.parentNode.insertBefore(script, entry);
     })();
+  }, []);
+
+  useEffect(() => {
+    (() => {
+      const s = document.createElement("script");
+      s.key = Math.random().toString();
+      s.innerHTML = erxesSettings;
+      const entry = document.getElementsByTagName("script")[0];
+      entry.parentNode.insertBefore(s, entry);
+    })();
+
+    if (settings.forms.length > 0) {
+      (() => {
+        const script = document.createElement("script");
+        script.src = formWidgetSource;
+        script.async = true;
+        script.key = Math.random().toString();
+        const entry = document.getElementsByTagName("script")[0];
+        entry.parentNode.insertBefore(script, entry);
+      })();
+    }
+  }, [erxesSettings, settings.forms.length]);
+
+  const queryParams = queryString.parse(location.search);
+  const kbTopic = getKbTopicQuery.widgetsKnowledgeBaseTopicDetail || {};
+
+  let marginTop = "main-body";
+  if (location.pathname === "/knowledge-base") {
+    headingSpacing = true;
+    marginTop = "mt-100p";
   }
 
-  render() {
-    const {
-      location,
-      getKbTopicQuery,
-      history,
-      children,
-      headingSpacing,
-    } = this.props;
-
-    const queryParams = queryString.parse(location.search);
-    const kbTopic = getKbTopicQuery.widgetsKnowledgeBaseTopicDetail || {};
-
-    return (
-      <div className="layout knowlegde-base">
-        <Header
-          history={history}
-          searchValue={queryParams.searchValue}
-          kbTopic={kbTopic}
-          headingSpacing={headingSpacing}
-        />
-        <Container className="main-body" fluid="lg">
-          {children}
-        </Container>
-        <Footer kbTopic={kbTopic} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="layout knowlegde-base">
+      <Header
+        history={history}
+        searchValue={queryParams.searchValue}
+        kbTopic={kbTopic}
+        headingSpacing={headingSpacing}
+      />
+      <Container className={marginTop} fluid="lg">
+        {children}
+      </Container>
+      <Footer kbTopic={kbTopic} />
+    </div>
+  );
+};
 
 export default withRouter(
   compose(
